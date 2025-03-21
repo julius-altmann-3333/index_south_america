4df29a4722f0796e8fe067c0668b4ab46949c7a3
julius-altmann-3333 <167089364+julius-altmann-3333@users.noreply.github.com>
2025-03-21 10:02:15 +0100

    Add files via upload

---
+++ dropDownSouthAmerica.js
@@ -0,0 +1,4 @@
+document.getElementById("toggleDropdownSouthAmerica").addEventListener("click", function() {
+    var dropdown_south_america = document.getElementById("dropdownContentSouthAmerica");
+    dropdown_south_america.style.display = (dropdown_south_america.style.display === "none" || dropdown_south_america.style.display === "") ? "block" : "none";
+});
